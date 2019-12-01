3cd58ac16822437c8faa0b72ed6d56055732f5b9
KushchO <lvioleg@gmail.com>
2019-12-01 16:37:26 +0300

    Debug

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 if (process.env.NODE_ENV === 'production') {
+  console.log('production');
   module.exports = {
     mongoURI:
-      'mongodb+srv://lvioleg:MLAolwwEpPZQ55p3@cluster0-lc718.gcp.mongodb.net/vidjot-prod?retryWrites=true&w=majority'
+      'mongodb+srv://lvioleg:MLAolwwEpPZQ55p3@cluster0-lc718.gcp.mongodb.net/test?retryWrites=true&w=majority'
   };
 } else {
   module.exports = {
