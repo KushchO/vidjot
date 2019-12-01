bb26a1a8eaffa7b30c88038f9d3b4e2ec67d45bb
KushchO <lvioleg@gmail.com>
2019-12-01 16:23:00 +0300

    обновил monodbURI

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     mongoURI:
-      'mongodb+srv://lvioleg:MLAolwwEpPZQ55p3@cluster0-lc718.gcp.mongodb.net/test?retryWrites=true&w=majority'
+      'mongodb+srv://lvioleg:MLAolwwEpPZQ55p3@cluster0-lc718.gcp.mongodb.net/vidjot-prod?retryWrites=true&w=majority'
   };
 } else {
   module.exports = {
